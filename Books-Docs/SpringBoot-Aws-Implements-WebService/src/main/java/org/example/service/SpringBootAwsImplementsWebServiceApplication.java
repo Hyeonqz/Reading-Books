a5341a87087c1ed75788d43015870b24671089e6
Hyeonqz <wlsgusrb78@naver.com>
2024-04-19 12:04:12 +0900

    webService init

---
+++ Books-Docs/SpringBoot-Aws-Implements-WebService/src/main/java/org/example/service/SpringBootAwsImplementsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsImplementsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsImplementsWebServiceApplication.class, args);
+	}
+
+}
