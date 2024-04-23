8702f9247f266a0f3de2f9392397228d771297da
Hyeonqz <wlsgusrb78@naver.com>
2024-04-23 12:19:13 +0900

    모던자바 5장 #3

---
+++ Books-Docs/Ex/src/ex1/Test.java
@@ -0,0 +1,36 @@
+package ex1;
+
+import static java.util.stream.Collectors.*;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Test {
+	public static void main (String[] args) {
+
+		Trader raoul = new Trader("Raoul", "Cambridge");
+		Trader mario = new Trader("Mario", "Milan");
+		Trader alan = new Trader("Alan", "Cambridge");
+		Trader brian = new Trader("Brian", "Cambridge");
+
+		List<Transaction> transactions = Arrays.asList(
+			new Transaction(brian, 2011, 300),
+			new Transaction(raoul, 2012, 1000),
+			new Transaction(raoul, 2011, 400),
+			new Transaction(mario, 2012, 710),
+			new Transaction(mario, 2012, 700),
+			new Transaction(alan, 2012, 950)
+		);
+
+		Optional<Transaction> min = transactions.stream()
+			.reduce((t1, t2) -> t1.getValue() < t2.getValue() ? t1 : t2);
+
+	}
+
+}
+
