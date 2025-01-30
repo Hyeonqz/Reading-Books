57ea97662b68bd134a9fccb03174a6f850ca4549
hkjin <wlsgusrb78@naver.com>
2025-01-30 23:55:09 +0900

    2장 아키텍쳐 개요

--- ddd-start/src/main/java/org/hyeonqz/dddstart/DddStartApplication.java
+++ ddd-start/src/main/java/org/hyeonqz/dddstart/DddStartApplication.java
@@ -3,8 +3,17 @@ package org.hyeonqz.dddstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
 public class DddStartApplication {
+
+	@PostConstruct
+	public void init() {
+		log.info("도메인 주도 개발 시작하기!");
+	}
 
 	public static void main (String[] args) {
 		SpringApplication.run(DddStartApplication.class, args);
